ce0c160338c6ffe7e729fce50ceda7d88b7a246d
shadowed1 <itszachg@gmail.com>
2025-11-06 20:47:09 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,26 +1,17 @@
 curl -L -o ~/MyFiles/Downloads/junest.zip https://github.com/fsquillace/junest/archive/refs/tags/7.4.10.zip
-sleep 1
-bsdtar -xf junest.zip
-sleep 0.2
+bsdtar -xf ~/MyFiles/Downloads/junest.zip
 sudo cp -r ~/MyFiles/Downloads/junest-7.4.10 /usr/local/junest_installer
-sleep 0.2
 sudo mkdir -p /usr/local/junest
-sleep 0.2
 sudo chown -R 1000:1000 /usr/local/junest_installer
-sleep 0.2
 sudo chown -R 1000:1000 /usr/local/junest
-sleep 0.2
 find /usr/local/junest_installer -type d -exec chmod +x {} \;
-sleep 0.2
 find /usr/local/junest_installer -type f -name "*.sh" -exec chmod +x {} \;
-sleep 0.2
 find /usr/local/junest_installer -type f -exec grep -q '^#!' {} \; -exec chmod +x {} \;
-sleep 0.2
 export JUNEST_HOME=/usr/local/junest
 export PATH="$PATH:/usr/local/junest:/usr/local/junest_installer"
-
 cd /usr/local/junest_installer/bin/
 ./junest setup
-
 export JUNEST_HOME=/usr/local/junest
-export PATH="$PATH:/usr/local/junest_installer/bin:$JUNEST_HOME/bin"
+export JUNEST_INSTALLER=/usr/local/junest_installer
+export PATH="$PATH:$JUNEST_INSTALLER/bin:$JUNEST_HOME/bin:$JUNEST_HOME/usr/bin_wrappers"
+#export LD_LIBRARY_PATH="$JUNEST_HOME/usr/lib:$JUNEST_HOME/usr/lib64:$LD_LIBRARY_PATH"

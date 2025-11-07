386500e7103ea5cc8644d1931b296a6c6668a020
shadowed1 <itszachg@gmail.com>
2025-11-06 20:52:27 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,7 +4,8 @@ bsdtar -xf ~/Downloads/junest.zip
 sleep 0.2
 sudo mkdir -p /usr/local/junest_installer
 sleep 0.2
-sudo cp -r ~/Downloads/junest-7.4.10 /usr/local/junest_installer
+sudo cp -r ~/Downloads/junest-7.4.10 /usr/local/
+sudo mv /usr/local/junest-7.4.10 junest_installer
 sleep 0.2
 sudo mkdir -p /usr/local/junest
 sleep 0.2
@@ -17,7 +18,6 @@ sleep 0.2
 find /usr/local/junest_installer -type f -name "*.sh" -exec chmod +x {} \;
 sleep 0.2
 find /usr/local/junest_installer -type f -exec grep -q '^#!' {} \; -exec chmod +x {} \;
-sleep 0.2
 export JUNEST_HOME=/usr/local/junest
 export PATH="$PATH:/usr/local/junest:/usr/local/junest_installer"
 cd /usr/local/junest_installer/bin/
